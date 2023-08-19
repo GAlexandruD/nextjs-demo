3aeb57b37f0d855c250c2174dddcb1ee57aee210
Alexandru Dragos <61319009+GAlexandruD@users.noreply.github.com>
2023-08-19 23:02:26 +0300

    Update index.tsx

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -13,6 +13,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js</a> on Docker Compose!
+          This was changed by me. 
         </h1>
 
         <p className={styles.description}>
